3c696dc2fb6814c238a989fcf5907edfba779e5f
hotianbexuanto <1772720865@qq.com>
2025-04-17 16:44:28 +0800

    初始化KernelSU钩子，导出KProbe变量并添加模块初始化函数

--- include/linux/ksu_hooks.h
+++ include/linux/ksu_hooks.h
@@ -9,11 +9,9 @@
 extern bool ksu_devpts_hook;
 
 /* KernelSU KProbes */
-#ifdef KSU_HOOK_WITH_KPROBES
 extern struct kprobe execve_kp;
 extern struct kprobe newfstatat_kp;
 extern struct kprobe faccessat_kp;
 extern struct kprobe pts_unix98_lookup_kp;
-#endif
 
 #endif /* _LINUX_KSU_HOOKS_H */ 
