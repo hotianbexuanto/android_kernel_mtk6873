6940d1ebb1a89c5d8ec084030ccee9193f94374d
hotianbexuanto <1772720865@qq.com>
2025-10-02 16:41:34 +0800

    refactor(ksu): update execveat tracepoint hook name for sucompat integration
    modify hook function names in execve call paths to properly distinguish sucompat behavior. adjusts both native and compat syscall entry points to use more specific hook identifiers.

--- fs/exec.c
+++ fs/exec.c
@@ -1892,7 +1892,7 @@ int do_execve(struct filename *filename,
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
 #if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
-    trace_ksu_trace_execveat_hook((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+    trace_ksu_trace_execveat_sucompat_hook((int *)AT_FDCWD, &filename, &argv, &envp, 0);
 #endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
@@ -1922,7 +1922,7 @@ static int compat_do_execve(struct filename *filename,
 		.ptr.compat = __envp,
 	};
 #if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
-    trace_ksu_trace_execveat_hook((int *)AT_FDCWD, &filename, &argv, &envp, 0); // 32-bit su and 32-on-64 support
+    trace_ksu_trace_execveat_sucompat_hook((int *)AT_FDCWD, &filename, &argv, &envp, 0); // 32-bit su and 32-on-64 support
 #endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
